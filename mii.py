dff2a86fc6fd5b78e4c47fe31e491c453f96f80a
wisdomenson <93410549+wisdomenson@users.noreply.github.com>
2022-08-10 12:31:09 +0000

    Update mii.py

--- mii.py
+++ mii.py
@@ -8,7 +8,7 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #image inversion
 inverted_image = 255 - gray_image
 
-blurred = cv2.GaussianBlur(inverted_image, (21, 21), )
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
 inverted_blurred = 255 - blurred
 pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
 
